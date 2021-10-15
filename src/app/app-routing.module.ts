d79311a9cd3d6bb0e6754c5f5332b122fb77c656
bangtur_studio <dani.widodo87@gmail.com>
2021-10-15 14:40:11 +0800

    add pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,22 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'article-details',
+    loadChildren: () => import('./article-details/article-details.module').then( m => m.ArticleDetailsPageModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
+  },
+  {
+    path: 'categories',
+    loadChildren: () => import('./categories/categories.module').then( m => m.CategoriesPageModule)
+  },
+  {
+    path: 'bookmark',
+    loadChildren: () => import('./bookmark/bookmark.module').then( m => m.BookmarkPageModule)
+  },
 ];
 
 @NgModule({
